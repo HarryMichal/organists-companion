a2280407e0f71158333f30694aaa16de7d460e81
HarryPhoon <harrymichal@seznam.cz>
2017-11-28 22:00:37 +0100

    Passport configuring, small restructure
    Some minor changes to passport and its configuration. Shouldn't be too
    complicated to complete the setup and make finally working
    authentication.
    
    Obvious problems:
      - In react on /register route try to send a form with credentials but
    the server returns 500
      - Other API calls regarding passport doesn't work too
    
    My problems:
      - Not sure what kind of structure should I use for passport.
      - Config file is a bit odd for me when similar code is present in
    /routes/main.js
    
    Need to figure this out

---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const sqlite3 = require('sqlite3');
+
+// ----------------------------------------------
+
+module.exports = router;
