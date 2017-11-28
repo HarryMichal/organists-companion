8bd9cf38a6b9cd002033737281f2b6cff44dd3af
HarryPhoon <harrymichal@seznam.cz>
2017-11-28 08:34:57 +0100

    Fixed a typo

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ var db = new sqlite3.Database('../db/testdb.db', sqlite3.OPEN_READWRITE, (err) =
   if (err) {
     console.error(err.message);
   }
-  console.log('Passport: Connected to the testdb file
+  console.log('Passport: Connected to the testdb file');
   });
 
 // expose this function to our app using module.exports
