9958228f49064d12acaa191c750b5dcc116c8445
HarryPhoon <harrymichal@seznam.cz>
2018-12-23 13:50:31 +0100

    Code cleanup
    I removed unused certificats because the app still doesn't use HTTPS.
    Finally I renamed www to www.js to be consistent.
    Also I removed some unsued files.

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'react-client/build', 'index.html'))
-})
-
-module.exports = router;
