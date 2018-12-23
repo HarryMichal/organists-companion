d3ef8cc2c346b58e4eca858c4a963a6f1cbab1d5
HarryPhoon <harrymichal@seznam.cz>
2018-12-23 13:54:23 +0100

    Package update
    I removed some unused packages and bumped few packages to a new version.
    Npm reported some vulneralibities so I let it do it's thing.
    Also adjusted start scripts to call www.js instead of www

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 var express = require('express');
 var path = require('path');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 
-var mainRouter = require('./routes/main');
 var apiRouter = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
